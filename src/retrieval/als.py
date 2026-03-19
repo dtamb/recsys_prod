149d2496c82e0925c7665e16ae8d6041de8d6df6
dtamb <89025060+dtamb@users.noreply.github.com>
2026-03-19 16:18:13 +1100

    refactor: centralise als functions
    Moved all ALS commands to central retrieval folder.
    Updated and tested 02_retrieval notebook. No functional changes.

--- src/retrieval/als.py
+++ src/retrieval/als.py
@@ -1,8 +1,29 @@
 # als_trainer.py
-
-
+from .post_processing import remove_duplicates
 from pyspark.ml.recommendation import ALS
-def train_als(train_df, config):
+
+def generate_candidates(model, train, k):
+    '''
+        Two-steps process for generating candidates.
+        1) Creating standard rec function using the model's method
+        2) Calling filter function to remove any recommended items that users have
+        already seen. (Although ALS' recommendForAllUsers removes already seen
+        films from the recommendations dataframe, some seen films do remain.)
+
+        Agrs:
+            - model: model generated from ALS training
+            - train: training data for duplicate comparison
+            - k: number of candidates (due to filtering, actual candidate number
+            will be <=k)
+        Returns:
+            List of recommendations per user, filtered for already seen items
+    '''
+    recs = model.recommendForAllUsers(k)
+    
+    return remove_duplicates(recs, train)
+
+
+def train(train_df, config):
     '''
     Args: train_df, config
         train_df: userId, itemId, rating
