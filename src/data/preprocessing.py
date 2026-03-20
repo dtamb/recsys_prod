8947da2a1ae3b0e019ed67532ee5b7a386d10fb0
dtamb <89025060+dtamb@users.noreply.github.com>
2026-03-20 11:47:34 +1100

    feat: add function to vectorise rows in Spark DF
    Converts selected rows in a Spark DF to a single vector column.

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -5,6 +5,8 @@ from pyspark.sql.functions import (
     first, sum, avg, min, max, count, countDistinct
 )
 
+from pyspark.ml.feature import VectorAssembler, StandardScaler
+
 def pivot_table(df, index_col, column_col, value_col, agg_func=first):
     '''
     Pivots a Spark DataFrame from long --> wide format.
@@ -21,3 +23,33 @@ def pivot_table(df, index_col, column_col, value_col, agg_func=first):
     
     '''
     return df.groupBy(index_col).pivot(column_col).agg(agg_func(value_col))
+
+
+def vectorise(df, exclude_cols, output_col='features'):
+    '''
+    Converts rows in a DataFrame into a single vector. 
+    Excluded columns determine non-vectorised rows.
+    
+    Args:
+        df: Spark DataFrame
+        exclude_cols: can be in one of two formats
+            - (str) column to exclude, e.g. 'movieId'
+            - (dict of str) columns to exclude, e.g. '['userId', 'movieId']'
+        output_col: (str) name of new vector column
+        
+    Returns:
+        Spark DataFrame with selected columns now as a single vector column
+    
+    '''
+    # Ensure exclude_cols is a list
+    if isinstance(exclude_cols, str):
+        exclude_cols = [exclude_cols]
+        
+    feature_cols = [c for c in df.columns if c not in exclude_cols]
+    
+    assembler = VectorAssembler(
+        inputCols = feature_cols,
+        outputCol = output_col
+    )
+    
+    return assembler.transform(df).select(*exclude_cols, output_col)
