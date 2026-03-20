1b4e5f6e10a763655838e8a8117864bca89db9d4
dtamb <89025060+dtamb@users.noreply.github.com>
2026-03-20 19:34:14 +1100

    feat: convert genome df to reduced PCA dimensions
    Converts the genome dataset into a vectorised and scaled dataframe.
    
    Runs PCA model to reduce dimensions and return both PCA and scalar model
    as well as final PCA-reduced genome data.

--- src/features/tag_features.py
+++ src/features/tag_features.py
@@ -1,12 +1,9 @@
 # item_features.py
 
-# Import generic pivot function
-from data.preprocessing import pivot_table
-
-# Import column names
+from data.preprocessing import pivot_table, vectorise, standardiser
+from features.pca import fit_pca
 import configs.settings as cfg
 
-# aggregate function options for pivot table function
 from pyspark.sql.functions import (
     first, sum, avg, min, max, count, countDistinct
 )
@@ -37,4 +34,37 @@ def pivot_genome_scores (
     
     pivoted = pivot_table(df, index_col, column_col, value_col, agg_func)
     return pivoted.fillna(fill_value).orderBy(index_col)
+    
+def build_genome_pca_features(genome_df, k=45, scale_std=False):
+    
+    '''
+    Creates scaler model + pca model and df for genome dataset.
+    
+    Args:
+        genome_df: Spark DataFrame
+        k: reduced dimensions for PCA
+        scale_std: flag for scaling standard deviation to 1
+        
+    Returns:
+        genome_scaler_model: scaler model trained on genome data
+        genome_pca_df: Spark DataFrame of reduced dimensions genome data
+        genome_pca_model: PCA model for reducing genome dimensions
+        
+    '''
+    
+    # Pivot and vectorise genome data (using default values)
+    vector_df = vectorise(pivot_genome_scores(genome_df), cfg.ITEM_COL)
+    
+    # Standardise and create scaler model
+    genome_std, genome_scaler_model = standardiser(vector_df, scale_std=scale_std)
+    
+    # Run PCA on genome dataset to create PCA dataset & model
+    genome_pca_df, genome_pca_model = fit_pca(genome_std, item_col=cfg.ITEM_COL,
+                                              k=k)
+    
+    return genome_scaler_model, genome_pca_df, genome_pca_model
+    
+    
+    
+    
     
