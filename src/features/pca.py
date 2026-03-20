e3875617e29507c35530c8799d7a60c5df09e194
dtamb <89025060+dtamb@users.noreply.github.com>
2026-03-20 17:12:32 +1100

    feat: add func to plot pca cumsum and save to disk
    Plots cumulative variance sum from compute_pca_cumsum over k dimensions.
    Also plots a percent threshold goal and can save plot image.

--- src/features/pca.py
+++ src/features/pca.py
@@ -2,6 +2,7 @@
 
 from pyspark.ml.feature import PCA
 import numpy as np
+import matplotlib.pyplot as plt
 
 def fit_pca(df_scaled, item_col, input_col="scaled_features", output_col="pca_features", k=100):
     
@@ -17,7 +18,8 @@ def fit_pca(df_scaled, item_col, input_col="scaled_features", output_col="pca_fe
         k (int): number of feature dimensions
     
     Returns (list):
-        pca_df: Spark DataFrame with a vector column containing k-dimension features
+        pca_df: Spark DataFrame with a vector column containing k-dimension
+            features
         pca_model: Fitted PCA Model
     '''
     
@@ -46,4 +48,34 @@ def compute_pca_cumsum(pca_model):
     return cumulative
         
     
+def plot_pca_cumsum(cumulative_variance, threshold=0.9, plot_save_path=''):
+    '''
+    Plots the cumulative variances and a threshold line for PCA. 
+    Option to save graph.
     
+    Args: 
+        cumulative_variance: explained variances over k for pca model (output from
+            compute_pca_cumsum)
+        threshold: percentage threshold goal for PCA
+        plot_save_path: path to save plot (if empty, will not save)
+        
+    Returns:
+        None
+    '''
+    
+    plt.figure(figsize=(8,5))
+    plt.plot(range(1, len(cumulative_variance)+1), cumulative_variance, marker='o')
+    plt.axhline(
+        y=threshold, color='r', linestyle='--',
+        label=f'{threshold*100:.0f}%threshold'
+    )
+    plt.xlabel('Number of components')
+    plt.ylabel('Cumulative explained variance')
+    plt.title('PCA Cumulative Explained Variance')
+    plt.grid(True)
+    plt.legend()
+    
+    # Saves plot if given file path 
+    if plot_save_path:
+        plt.savefig(plot_save_path, dpi=300)
+    plt.show()
