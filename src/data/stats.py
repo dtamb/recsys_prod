40aaa44d383ed688e67ac3295dac373b55821a48
dtamb <89025060+dtamb@users.noreply.github.com>
2026-04-23 14:46:18 +1000

    docs: add docstring for global mean function

--- src/data/stats.py
+++ src/data/stats.py
@@ -4,7 +4,17 @@ from pyspark.sql import functions as F
 import configs.settings as cfg
 
 def compute_global_mean(ratings_df, rating_col=cfg.RATING_COL):
-
+    '''
+    Computes the global average of all ratings in the ratings dataset.
+    
+    Args:
+        ratings_df: Spark DataFrame with ratings data
+        rating_col: column to average over
+        
+    Output:
+        mu: global average of ratings
+        
+    '''
     mu = ratings_df.select(F.avg(rating_col).alias("mu")).first()["mu"]
     
     return mu
