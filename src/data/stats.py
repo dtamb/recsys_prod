72a0977512a8e7ef97d465b642f9bded1b9b9477
dtamb <89025060+dtamb@users.noreply.github.com>
2026-04-24 17:29:03 +1000

    feat: add global standard deviation function
    Takes rating dataframe and computes the global standard deviation of
    the entire ratings dataset.

--- src/data/stats.py
+++ src/data/stats.py
@@ -18,3 +18,21 @@ def compute_global_mean(ratings_df, rating_col=cfg.RATING_COL):
     mu = ratings_df.select(F.avg(rating_col).alias("mu")).first()["mu"]
     
     return mu
+
+def compute_global_std(ratings_df, rating_col=cfg.RATING_COL):
+    
+    '''
+    Computes the global standard deviation of all ratings in the ratings dataset.
+    
+    Args:
+        ratings_df: Spark DataFrame with ratings data
+        rating_col: column to average over
+        
+    Output:
+        global_std: global standard deviation
+        
+    ''' 
+    
+    global_std = ratings_df.select(F.stddev(rating_col).alias('global_std')).first()['global_std']
+    
+    return global_std
