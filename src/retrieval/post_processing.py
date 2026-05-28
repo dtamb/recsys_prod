e72872d857ff5e1fa8395515d75d9a3ef05c8276
dtamb <89025060+dtamb@users.noreply.github.com>
2026-05-28 18:11:34 +1000

    fix: optimise redundancy removal
    Changed array exploder to be after duplicates are removed. Built code to
    remove duplicates from an array for optimal code.

--- src/retrieval/post_processing.py
+++ src/retrieval/post_processing.py
@@ -4,6 +4,7 @@
 
 from pyspark.sql import functions as F
 from pyspark.sql.window import Window
+import configs.settings as cfg
 
 def remove_duplicates(df_rec, df_seen, k):
     '''
@@ -30,29 +31,41 @@ def remove_duplicates(df_rec, df_seen, k):
         
     '''
     
-    # Expand out recommendations array and structure
-    exploded = df_rec.select(
-        df_rec.userId,
-        F.explode('recommendations').alias('rec')
-    ).select(
-        'userId',
-        'rec.*'
-    )
+    # Create df of seen films and group into sets
+    seen = df_seen.select(cfg.USER_COL, cfg.ITEM_COL).distinct()
 
-    # Remove recommendations that are already seen
-    filtered = exploded.join(
-        df_seen.select('userId', 'movieId'),
-        on=['userId', 'movieId'],
-        how='left_anti'
-    ).orderBy(
-        ['userId', 'rating'],
-        ascending=[True, False]
+    seen_grouped = seen.groupBy(cfg.USER_COL).agg(
+        F.collect_set(cfg.ITEM_COL).alias("seen_items")
     )
     
+    # Create data frame with both seen films and recommendation films
+    df_with_seen = df_rec.join(
+        F.broadcast(seen_grouped),
+        on=cfg.USER_COL,
+        how="left"
+    )
+    
+    # Filtering out films already seen
+    filtered_arrays = df_with_seen.select(
+        cfg.USER_COL,
+        F.expr("""
+            filter(recommendations, r -> NOT array_contains(seen_items, r.movieId))
+        """).alias("filtered_recs")
+    )
+    
+   # Expand out recommendations array and structure
+    exploded = filtered_arrays.select(
+        cfg.USER_COL,
+        F.explode("filtered_recs").alias("rec")
+    ).select(
+        cfg.USER_COL,
+        "rec.*"
+    )
+    
+    
     # Create window function to  re-rank and select top k values
-    window = Window.partitionBy("userId").orderBy(F.desc("rating"))
-
-    filtered = filtered.withColumn(
+    window = Window.partitionBy(cfg.USER_COL).orderBy(F.desc(cfg.RATING_COL))
+    filtered = exploded.withColumn(
         "rank", F.row_number().over(window)
     ).filter(
         F.col("rank") <= k
