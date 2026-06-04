07dc88e945c49f4f0a3fb96e1047952027c4665f
dtamb <89025060+dtamb@users.noreply.github.com>
2026-06-04 18:14:48 +1000

    docs: add docstring

--- src/ranking/ranking_df.py
+++ src/ranking/ranking_df.py
@@ -5,12 +5,34 @@ from pyspark.sql import functions as F
 
 def create_ranking_df(similarity_score, user_feature, item_feature, user_bias, item_bias):
     '''
-    Merges all features into a single dataframe for ranking 
+    Merges all features into a single dataframe for ranking.
+    
+    ranking DF schema
+    root
+     |-- movieId: integer (nullable = true)
+     |-- userId: integer (nullable = true)
+     |-- als_score: float (nullable = true)
+     |-- similarity: double (nullable = true)
+     |-- user_avg_rating: double (nullable = true)
+     |-- user_rating_std: double (nullable = false)
+     |-- user_log_rating_count: double (nullable = true)
+     |-- days_since_last_activity: integer (nullable = true)
+     |-- user_bayes_std: double (nullable = true)
+     |-- item_avg_rating: double (nullable = true)
+     |-- item_bayesian_avg: double (nullable = true)
+     |-- item_log_rating_count: double (nullable = true)
+     |-- user_bias: double (nullable = true)
+     |-- item_bias: double (nullable = true)
     
     Args:
-    
+        similarity_score --> ranking.tag_similarity : compute_tag_similarity
+        user_feature --> features.user_features : build_user_features
+        item_feature --> features.item_features : build_item_features
+        user_bias --> features.biases : compute_user_bias
+        item_bias --> features.biases : compuute_item_bias
     
     Returns:
+        ranking: Spark DF with full set of features for ranking
     
     '''
     
